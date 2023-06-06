e80729867a12a4a9f6c5d1834fc86bd5068d98f7
Emergency1999 <johannes@schmid-company.de>
2023-06-06 13:44:37 +0200

    0.5.0: #17 separated unauthorized and authorized

--- src/functions/hello_world.ts
+++ src/functions/hello_world.ts
@@ -1,7 +1,7 @@
 import { Template } from "../generated/interfaces/index.js";
 import { TemplateSchema } from "../schemas/schemas.js";
 import { TemplateResult } from "../resultTypes.js";
-import { FunctionServer, HttpMethod } from "../types.js";
+import { UnauthenticatedFunctionServer, HttpMethod } from "../types.js";
 import { AuthLevel } from "../types.js";
 
 export type RequestInterface = Template;
@@ -10,8 +10,8 @@ export type ResponseType = TemplateResult;
 export const method: HttpMethod = "POST";
 export const authLevel: AuthLevel = AuthLevel.anonymous;
 
-type Function = FunctionServer<RequestInterface, ResponseType>;
-export const hello_world: Function = async ({}, user): ReturnType<Function> => {
+type Function = UnauthenticatedFunctionServer<RequestInterface, ResponseType>;
+export const hello_world: Function = async ({}): ReturnType<Function> => {
   return {
     status: 200,
     body: "Hello World!",
--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,18 @@
-import { Connection } from "@planetscale/database";
 import { UserData } from "./generated/interfaces/index.js";
-import { FunctionResponse, FunctionResults } from "./resultTypes.js";
-import { Redis } from "@upstash/redis";
+import { FunctionResponse, FunctionResults } from "./resultTypes";
 
 export { HttpMethod } from "@azure/functions";
 
 export type FunctionRequest = {};
 
-export type FunctionServer<
+export type AuthenticatedFunctionServer<
   REQ extends FunctionRequest,
   RES extends FunctionResults
-> = (body: REQ, user?: UserData) => Promise<FunctionResponse<RES>>;
+> = (body: REQ, user: UserData) => Promise<FunctionResponse<RES>>;
+export type UnauthenticatedFunctionServer<
+  REQ extends FunctionRequest,
+  RES extends FunctionResults
+> = (body: REQ) => Promise<FunctionResponse<RES>>;
 
 export enum AuthLevel {
   /**
@@ -26,13 +28,3 @@ export enum AuthLevel {
    */
   admin = "admin",
 }
-
-export type ServiceType = "planetscale" | "redis"; // | "idot";
-
-export type Services<T extends ServiceType[]> = {
-  [K in T[number]]: K extends "planetscale"
-    ? Connection
-    : K extends "redis"
-    ? Redis
-    : never;
-};
